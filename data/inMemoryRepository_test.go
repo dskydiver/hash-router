651f286084f6f242ad36cd99e03531ec2a5fe60c
dskydiver <dastanmukhan6@gmail.com>
2022-09-07 16:56:40 +0430

    Update Tests for InMemoryRepository

--- data/inMemoryRepository_test.go
+++ data/inMemoryRepository_test.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"gitlab.com/TitanInd/hashrouter/interfaces"
@@ -17,11 +18,13 @@ func (t *TestModel) SetID(id string) interfaces.IBaseModel {
 	return t
 }
 
-func (t *TestModel) Test() {}
+func (t *TestModel) Test() error{
+	return nil
+}
 
 type ITestModel interface {
 	interfaces.IBaseModel
-	Test()
+	Test() error
 }
 
 var transactionsChannel chan func()
@@ -30,6 +33,40 @@ func TestNewInMemoryRepository(t *testing.T) {
 	transactionsChannel = NewTransactionsChannel()
 
 	repo := NewInMemoryRepository[ITestModel](nil, NewInMemoryDataStore(), transactionsChannel)
+	model, err := repo.Create(&TestModel{})
+	require.Nil(t, err)
 
+	err=model.Test()
+	require.Nil(t, err)
+
+	id:=model.GetID()
+	require.Equal(t, id,"testid")
+
+
+	_,err=repo.Save(model)
+	require.Nil(t, err)
+
+	_,err=repo.Get("testid213")
+	require.NotNil(t, err)
+
+	item,err:=repo.Update(model)
+	require.Nil(t, err)
+	require.Equal(t, item,model)
+
+	item,err=repo.Delete(model)
+	require.Nil(t, err)
+	require.Equal(t, item,nil)
+
+	_,err=repo.FindOne(model)
+	require.Nil(t, err)
+
+
+	// TODO: Update with better scenario
+	items:=repo.Query(model)
+	require.Nil(t, items)
+
+<<<<<<< Updated upstream
 	repo.Create(&TestModel{})
+=======
+>>>>>>> Stashed changes
 }
