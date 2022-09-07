0a6b11ed1dec2a15a9c6c374b963ed4a572f1178
dskydiver <dastanmukhan6@gmail.com>
2022-09-07 17:55:44 +0430

    Add hashrate tests

--- hashrate/hashrate_test.go
+++ hashrate/hashrate_test.go
@@ -2,6 +2,7 @@ package hashrate
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 
@@ -9,15 +10,13 @@ import (
 )
 
 func TestHashrate(t *testing.T) {
-	t.Skip()
-
 	log, _ := zap.NewDevelopment()
-	hashrate := NewHashrate(log.Sugar())
+	hashrate := NewHashrate(log.Sugar(),time.Second)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 5; i++ {
 		hashrate.OnSubmit(10000)
-		fmt.Printf("Current Hashrate %d\n", hashrate.GetHashrate())
+		fmt.Printf("Current Hashrate %d\n", hashrate.getHashrateHS())
 		time.Sleep(1 * time.Second)
 	}
-
+	require.Equal(t, hashrate.GetTotalHashes(),uint64(50000))
 }
