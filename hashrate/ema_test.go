d01520af1260efe3a40b6f2674dd240696a29cfb
dskydiver <dastanmukhan6@gmail.com>
2022-09-07 18:09:11 +0430

    Fix lint issue

--- hashrate/ema_test.go
+++ hashrate/ema_test.go
@@ -1,27 +1,27 @@
 package hashrate
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestEma(t *testing.T) {
 	counter := New(time.Second)
 	counter.Add(10.0)
-	require.LessOrEqual(t, counter.Value(),float64(10.0))
-	val:=counter.ValuePer(time.Second)
-	require.Less(t, val,float64(10))
-	val=counter.LastValuePer(time.Second)
-	require.LessOrEqual(t, val,float64(10))
+	require.LessOrEqual(t, counter.Value(), float64(10.0))
+	val := counter.ValuePer(time.Second)
+	require.Less(t, val, float64(10))
+	val = counter.LastValuePer(time.Second)
+	require.LessOrEqual(t, val, float64(10))
 
 	counter.Add(20.0)
-	val=counter.Value()
-	require.Greater(t, val,float64(29))
-	require.Less(t, val,float64(30))
+	val = counter.Value()
+	require.Greater(t, val, float64(29))
+	require.Less(t, val, float64(30))
 
-	val=counter.valueAfter(time.Second)
-	require.Greater(t, val,float64(10))
-	require.Less(t, val,float64(12))
-
+	val = counter.valueAfter(time.Second)
+	require.Greater(t, val, float64(10))
+	require.Less(t, val, float64(12))
 }
--- hashrate/hashrate_test.go
+++ hashrate/hashrate_test.go
@@ -2,21 +2,22 @@ package hashrate
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 
 	"go.uber.org/zap"
 )
 
 func TestHashrate(t *testing.T) {
 	log, _ := zap.NewDevelopment()
-	hashrate := NewHashrate(log.Sugar(),time.Second)
+	hashrate := NewHashrate(log.Sugar(), time.Second)
 
 	for i := 0; i < 5; i++ {
 		hashrate.OnSubmit(10000)
 		fmt.Printf("Current Hashrate %d\n", hashrate.getHashrateHS())
 		time.Sleep(1 * time.Second)
 	}
-	require.Equal(t, hashrate.GetTotalHashes(),uint64(50000))
+	require.Equal(t, hashrate.GetTotalHashes(), uint64(50000))
 }
