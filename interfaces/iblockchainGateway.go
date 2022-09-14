7576464896292d9da1bb8b89ee740aefbf527a4a
dskydiver <dastanmukhan6@gmail.com>
2022-09-13 21:08:37 -0700

    added blockchain interfaces

---
+++ interfaces/iblockchainGateway.go
@@ -0,0 +1,28 @@
+package interfaces
+
+import (
+	"context"
+	"math/big"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"gitlab.com/TitanInd/hashrouter/interop"
+)
+
+type IBlockchainGateway interface {
+	SubscribeToContractCreatedEvent(ctx context.Context) (chan types.Log, interop.BlockchainEventSubscription, error)
+
+	// SubscribeToContractEvents returns channel with events for particular contract
+	SubscribeToContractEvents(ctx context.Context, contractAddress common.Address) (chan types.Log, ethereum.Subscription, error)
+
+	// ReadContract reads contract information encoded in the blockchain
+	ReadContract(contractAddress common.Address) (interface{}, error)
+
+	ReadContracts(sellerAccountAddr interop.BlockchainAddress) ([]interop.BlockchainAddress, error)
+
+	// SetContractCloseOut closes the contract with specified closeoutType
+	SetContractCloseOut(fromAddress string, contractAddress string, closeoutType int64) error
+
+	GetBalanceWei(ctx context.Context, addr common.Address) (*big.Int, error)
+}
